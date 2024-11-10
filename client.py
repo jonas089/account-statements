acd5ab6baac6e120284375c116b9a463f6ee1c77
jonas pauli <jonaspauli089@gmail.com>
2024-11-10 12:00:44 +0100

    wip: client

--- client.py
+++ client.py
@@ -1,5 +1,22 @@
-def main():
-    pass
+from lib import *
+from cs import sum_merchant_MMYYYY
 
+revolut_mmyyyy = {
+    '01': 'january',
+    '02': 'february',
+    '03': 'march',
+    '04': 'april',
+    '05': 'may',
+    '06': 'june',
+    '07': 'july',
+    '08': 'august',
+    '09': 'september'
+}
+
+def main():
+    mmyyyy = input("Enter MMYYY (example 092024, September 2024): ")
+    # Description is Merchant Description for Revolut
+    revolut_parser = Parser('Description', None)
+    
 
 main()
